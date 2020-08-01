217e77b6a19db9a82ad1f746a8c9c41f63eff8b4
Rafi Priatna K <stormrider136@gmail.com>
2020-08-01 15:04:32 +0700

    rename

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,7 +20,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $title = "Products";
+        $title = "Product List";
 
         $items = Product::with([
             'category'
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,20 +15,41 @@
         >
           <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         </li>
-        <li class="menu-header">Administrator</li>
-        <li
-          @if (Request::url() === url('/admin/product'))
-            class="active"
-          @endif
-        >
-          <a class="nav-link" href="{{ route('product.index') }}"><i class="fas fa-box"></i> <span>Products</span></a>
+        <li class="menu-header">
+          <li
+            @if (Request::url() === url('/admin/product') || Request::url() === url('/admin/product-category'))
+                class="dropdown active"
+            @else
+                class="dropdown"
+            @endif
+          >
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+              <i class="fas fa-box"></i> <span>Products</span>
+            </a>
+            <ul class="dropdown-menu">
+
+              <li
+                @if (Request::url() === url('/admin/product'))
+                  class="active"
+                @endif
+              >
+                <a class="nav-link" href="{{ route('product.index') }}">
+                  <i class="fas fa-list"></i> <span>Product List</span></a>
+              </li>
+
+              <li
+                @if (Request::url() === url('/admin/product-category'))
+                  class="active"
+                @endif
+              >
+                <a class="nav-link" href="{{ route('product-category.index') }}">
+                  <i class="fas fa-tags"></i> <span>Product Categories</span>
+                </a>
+              </li>
+
+            </ul>
+          </li>
         </li>
-        <li
-          @if (Request::url() === url('/admin/product-category'))
-            class="active"
-          @endif
-        >
-          <a class="nav-link" href="{{ route('product-category.index') }}"><i class="fas fa-tags"></i> <span>Product Categories</span></a></li>
         </ul>   
     </aside>
 </div>
