5e09f29e44edab2ab970b4f39baa45569642f2b8
Rafi Priatna K <stormrider136@gmail.com>
2020-08-11 09:21:15 +0700

    added transaction sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -63,6 +63,31 @@
         <li class="{{ (Request::url() === route('customer.index')) ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('customer.index') }}"><i class="fas fa-users"></i> <span>Customers</span></a>
         </li>
+
+        <li>
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+            <i class="fas fa-shopping-cart"></i> <span>Transaction</span>
+          </a>
+          <ul class="dropdown-menu">
+
+            <li>
+              <a class="nav-link" href="#">
+                <i class="fas fa-cart-plus"></i> <span>Make Transaction</span></a>
+            </li>
+            
+            <li>
+              <a class="nav-link" href="#">
+                <i class="fas fa-list-alt"></i> <span>Transaction List</span></a>
+            </li>
+
+            <li>
+              <a class="nav-link" href="#">
+                <i class="far fa-chart-bar"></i> <span>Transaction Report</span></a>
+            </li>
+
+          </ul>
+        </li>
+        
       </li>
 
       </ul>   
